05086a12468f202a121013c97ae7a9d8b4cb3272
zimlee0621 <123623639+zimlee0621@users.noreply.github.com>
2023-02-28 15:53:59 +0800

    Create pullRequest_lim.c

---
+++ pullRequest_lim.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Programmer 2. This part must be used for the pull request.
+int sum(int num1, int num2) {
+    return num1 + num2;
+}
+
+// Programmer 1. This part must be used for the main branch. 
+int main() {
+    int num1=1, num2=2;
+    
+    printf("The sum of %d and %d is %d\n", num1, num2, result);
+    
+    return 0;
+}
