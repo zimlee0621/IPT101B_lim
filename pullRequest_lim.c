c81ae2e89ed462f155037ea00fe452875ce47e75
zimlee0621 <123623639+zimlee0621@users.noreply.github.com>
2023-02-28 15:56:16 +0800

    Update pullRequest_lim.c

--- pullRequest_lim.c
+++ pullRequest_lim.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-
-// Programmer 2. This part must be used for the pull request.
-int sum(int num1, int num2) {
-    return num1 + num2;
-}
+#include <stdio.h
 
 // Programmer 1. This part must be used for the main branch. 
 int main() {
